1df277ec00808bf2b42df79fa013fd38846056cf
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-07-07 18:34:22 +0500

    task changed

--- main/tasks.py
+++ main/tasks.py
@@ -1,13 +1,23 @@
 from celery import shared_task
 from django.core import management
 from django.utils import timezone
+from io import StringIO
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def export_monthly_applications_task():
     now = timezone.localtime()
-    print(f"[{now.isoformat()}] Running export_monthly_applications_task")
-    management.call_command("export_monthly_applications")
+    logger.info(f"[{now.isoformat()}] Running export_monthly_applications_task")
+    out = StringIO()
+    management.call_command("export_monthly_applications", stdout=out)
+    result = out.getvalue().strip()
+    # natijani ham log qilamiz
+    logger.info(f"export_monthly_applications command output: {result}")
+    return result  # natijani task return sifatida qaytaramiz
+
 
 
 @shared_task
