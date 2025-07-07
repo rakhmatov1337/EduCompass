d9e4ae89b5eddcd0b1e2712c432aa844ff9c6069
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-07-07 17:47:34 +0500

    changed

--- educompass/celery.py
+++ educompass/celery.py
@@ -1,9 +1,12 @@
 import os
-
+import logging
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educompass.settings")
 
-app = Celery("educompass") 
+app = Celery("educompass")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+logging.getLogger("celery.beat").info(
+    f"Loaded beat_schedule: {app.conf.beat_schedule!r}")
