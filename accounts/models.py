eada43c6daf37a3b0dc4b14a579cd09547c344ed
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-07-12 17:20:26 +0500

    Center payment added

--- accounts/models.py
+++ accounts/models.py
@@ -105,6 +105,10 @@ class CenterPayment(models.Model):
         on_delete=models.CASCADE,
         related_name='payment'
     )
+    paid_amount = models.DecimalField(
+        max_digits=12, decimal_places=2,
+        default=0
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
